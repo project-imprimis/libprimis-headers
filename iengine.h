be96ef394c0ac6c1929ff802b5f4c64eccccbf08
Alex Foster <123afoster@gmail.com>
2020-12-28 15:46:08 -0800

    remove moveplayer/modifygravity() fxns not in engine

--- iengine.h
+++ iengine.h
@@ -462,8 +462,6 @@ extern int numdynents;
 extern vector<dynent *> dynents;
 
 extern dynent *iterdynents(int i);
-extern void moveplayer(physent *pl, int moveres, bool local);
-extern bool moveplayer(physent *pl, int moveres, bool local, int curtime);
 extern void crouchplayer(physent *pl, int moveres, bool local);
 extern bool collide(physent *d, const vec &dir = vec(0, 0, 0), float cutoff = 0.0f, bool playercol = true, bool insideplayercol = false);
 extern bool bounce(physent *d, float secs, float elasticity, float waterfric, float grav);
@@ -481,7 +479,6 @@ extern void updatedynentcache(physent *d);
 extern bool entinmap(dynent *d, bool avoidplayers = false);
 extern void findplayerspawn(dynent *d, int forceent = -1, int tag = 0);
 
-extern void modifygravity(physent *pl, bool water, int curtime);
 extern void modifyvelocity(physent *pl, bool local, bool water, bool floating, int curtime);
 extern void recalcdir(physent *d, const vec &oldvel, vec &dir);
 extern void slideagainst(physent *d, vec &dir, const vec &obstacle, bool foundfloor, bool slidecollide);
