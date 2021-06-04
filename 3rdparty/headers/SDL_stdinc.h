5fe432dd671cea726abb24dd3e4e2c011e1ecc50
David Chen <45245358+TheEgghead27@users.noreply.github.com>
2021-06-04 16:43:05 -0400

    remove unnecessary fallthrough
    im am stupid

--- 3rdparty/headers/SDL_stdinc.h
+++ 3rdparty/headers/SDL_stdinc.h
@@ -452,7 +452,7 @@ SDL_FORCE_INLINE void SDL_memset4(void *dst, Uint32 val, size_t dwords)
         case 0: do {    *_p++ = _val; [[fallthrough]];   /* fallthrough */
         case 3:         *_p++ = _val; [[fallthrough]];   /* fallthrough */
         case 2:         *_p++ = _val; [[fallthrough]];   /* fallthrough */
-        case 1:         *_p++ = _val; [[fallthrough]];   /* fallthrough */
+        case 1:         *_p++ = _val;                    /* fallthrough */
         } while ( --_n );
     }
 #endif
