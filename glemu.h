7f352998488902d1a69d6874e29a809064529e7c
Alex Foster <123afoster@gmail.com>
2022-02-01 20:23:02 -0800

    remove unused glemu entries

--- glemu.h
+++ glemu.h
@@ -31,60 +31,22 @@ namespace gle
     extern void begin(GLenum mode);
     extern void begin(GLenum mode, int numverts);
     extern void multidraw();
-    extern void defattribs(const char *fmt);
     extern void defattrib(int type, int size, int format);
 
     extern void defvertex(int size = 3, int format = GL_FLOAT);
     extern void defcolor(int size = 3, int format = GL_FLOAT);
     extern void deftexcoord0(int size = 2, int format = GL_FLOAT);
-    extern void deftexcoord1(int size = 2, int format = GL_FLOAT);
     extern void defnormal(int size = 3, int format = GL_FLOAT);
-    extern void deftangent(int size = 4, int format = GL_FLOAT);
-    extern void defboneweight(int size = 4, int format = GL_UNSIGNED_BYTE);
-    extern void defboneindex(int size = 4, int format = GL_UNSIGNED_BYTE);
 
-    extern void vertexf(float x);
-    extern void vertexf(float x, float y);
-    extern void vertexf(float x, float y, float z);
-    extern void vertexf(float x, float y, float z, float w);
-    extern void vertex(const vec &v);
-    extern void vertex(const vec &v, float w);
-    extern void vertex(const vec2 &v);
-
-    extern void colorf(float x);
-    extern void colorf(float x, float y);
     extern void colorf(float x, float y, float z);
     extern void colorf(float x, float y, float z, float w);
 
     extern void color(const vec &v);
     extern void color(const vec &v, float w);
-    extern void color(const vec2 &v);
 
     extern void colorub(uchar x, uchar y, uchar z, uchar w = 255);
     extern void color(const bvec &v, uchar alpha = 255);
-    extern void texcoord0f(float x);
-    extern void texcoord0f(float x, float y);
-    extern void texcoord0f(float x, float y, float z);
-    extern void texcoord0f(float x, float y, float z, float w);
 
-    extern void texcoord0(const vec &v);
-    extern void texcoord0(const vec &v, float w);
-    extern void texcoord0(const vec2 &v);
-    extern void texcoord0(const vec4<float> &v);
-    extern void texcoord1f(float x);
-    extern void texcoord1f(float x, float y);
-    extern void texcoord1f(float x, float y, float z);
-    extern void texcoord1f(float x, float y, float z, float w);
-    extern void texcoord1(const vec &v);
-    extern void texcoord1(const vec &v, float w);
-    extern void texcoord1(const vec2 &v);
-    extern void normal(float x, float y, float z);
-    extern void normal(const vec &v);
-    extern void tangent(float x, float y, float z, float w = 1.0f);
-    extern void tangent(const vec &v, float w = 1.0f);
-
-    extern void enableattrib(int index);
-    extern void disableattrib(int index);
     extern void enablevertex();
     extern void disablevertex();
     extern void vertexpointer(int stride, const void *data, GLenum type = GL_FLOAT, int size = 3, GLenum normalized = GL_FALSE);
@@ -94,9 +56,6 @@ namespace gle
     extern void enabletexcoord0();
     extern void disabletexcoord0();
     extern void texcoord0pointer(int stride, const void *data, GLenum type = GL_FLOAT, int size = 2, GLenum normalized = GL_FALSE);
-    extern void enabletexcoord1();
-    extern void disabletexcoord1();
-    extern void texcoord1pointer(int stride, const void *data, GLenum type = GL_FLOAT, int size = 2, GLenum normalized = GL_FALSE);
     extern void enablenormal();
     extern void disablenormal();
     extern void normalpointer(int stride, const void *data, GLenum type = GL_FLOAT, int size = 3, GLenum normalized = GL_TRUE);
@@ -115,47 +74,14 @@ namespace gle
     extern void bindvbo(GLuint vbo);
     extern void clearvbo();
 
-    extern void attribf(float x);
     extern void attribf(float x, float y);
     extern void attribf(float x, float y, float z);
-    extern void attribf(float x, float y, float z, float w);
-    extern void attribd(double x);
-    extern void attribd(double x, double y);
-    extern void attribd(double x, double y, double z);
-    extern void attribd(double x, double y, double z, double w);
-    extern void attribb(char x);
-    extern void attribb(char x, char y);
-    extern void attribb(char x, char y, char z);
-    extern void attribb(char x, char y, char z, char w);
-    extern void attribub(uchar x);
-    extern void attribub(uchar x, uchar y);
-    extern void attribub(uchar x, uchar y, uchar z);
+
     extern void attribub(uchar x, uchar y, uchar z, uchar w);
-    extern void attribs(short x);
-    extern void attribs(short x, short y);
-    extern void attribs(short x, short y, short z);
-    extern void attribs(short x, short y, short z, short w);
-    extern void attribus(ushort x);
-    extern void attribus(ushort x, ushort y);
-    extern void attribus(ushort x, ushort y, ushort z);
-    extern void attribus(ushort x, ushort y, ushort z, ushort w);
-    extern void attribi(int x);
-    extern void attribi(int x, int y);
-    extern void attribi(int x, int y, int z);
-    extern void attribi(int x, int y, int z, int w);
-    extern void attribui(uint x);
-    extern void attribui(uint x, uint y);
-    extern void attribui(uint x, uint y, uint z);
-    extern void attribui(uint x, uint y, uint z, uint w);
 
     extern void attrib(const vec &v);
     extern void attrib(const vec &v, float w);
     extern void attrib(const vec2 &v);
-    extern void attrib(const ivec &v);
-    extern void attrib(const ivec &v, int w);
-    extern void attrib(const ivec2 &v);
-    extern void attrib(const bvec &b);
-    extern void attrib(const bvec &b, uchar w);
 
     extern int end();
 
