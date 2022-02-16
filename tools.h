09e7888059d3ff31b78e781066641aac3f679050
Alex Foster <123afoster@gmail.com>
2022-02-15 23:50:02 -0800

    use nullptr for DELETE macros

--- tools.h
+++ tools.h
@@ -98,8 +98,9 @@ inline float detrnd(uint s, int x)
     return static_cast<int>(((s*1103515245+12345)>>16)%x);
 }
 
-#define DELETEP(p) if(p) { delete   p; p = 0; }
-#define DELETEA(p) if(p) { delete[] p; p = 0; }
+//operator delete/delete[] macros
+#define DELETEP(p) if(p) { delete   p; p = nullptr; }
+#define DELETEA(p) if(p) { delete[] p; p = nullptr; }
 
 constexpr double SQRT2 = 1.4142135623731;
 constexpr double SQRT3 = 1.73205080756888;
