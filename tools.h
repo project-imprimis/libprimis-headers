41d75a4ec66a865e3e88f719ed4d1db7aab2af76
Alex Foster <123afoster@gmail.com>
2022-02-27 20:49:29 -0800

    remove queue/reversequeue/endianswap from tools.h

--- tools.h
+++ tools.h
@@ -2030,91 +2030,14 @@ struct hashtable : hashbase<hashtable<K, T>, hashtableentry<K, T>, K, T>
         } \
     }
 
-template <class T, int SIZE>
-struct queue
-{
-    int head, tail, len;
-    T data[SIZE];
-
-    queue() { clear(); }
-
-    void clear() { head = tail = len = 0; }
-
-    int length() const { return len; }
-    bool empty() const { return !len; }
-    bool full() const { return len == SIZE; }
-
-    bool inrange(size_t i) const { return i<size_t(len); }
-    bool inrange(int i) const { return i>=0 && i<len; }
-
-    T &added() { return data[tail > 0 ? tail-1 : SIZE-1]; }
-    T &added(int offset) { return data[tail-offset > 0 ? tail-offset-1 : tail-offset-1 + SIZE]; }
-    T &adding() { return data[tail]; }
-    T &adding(int offset) { return data[tail+offset >= SIZE ? tail+offset - SIZE : tail+offset]; }
-    T &add()
-    {
-        T &t = data[tail];
-        tail++;
-        if(tail >= SIZE) tail -= SIZE;
-        if(len < SIZE) len++;
-        return t;
-    }
-    T &add(const T &e) { return add() = e; }
-
-    T &pop()
-    {
-        tail--;
-        if(tail < 0)
-        {
-            tail += SIZE;
-        }
-        len--;
-        return data[tail];
-    }
-
-    T &removing() { return data[head]; }
-    T &removing(int offset) { return data[head+offset >= SIZE ? head+offset - SIZE : head+offset]; }
-    T &remove()
-    {
-        T &t = data[head];
-        head++;
-        if(head >= SIZE)
-        {
-            head -= SIZE;
-        }
-        len--;
-        return t;
-    }
-
-    T &operator[](int offset) { return removing(offset); }
-    const T &operator[](int offset) const { return removing(offset); }
-};
-
-template <class T, int SIZE>
-struct reversequeue : queue<T, SIZE>
-{
-    T &operator[](int offset) { return queue<T, SIZE>::added(offset); }
-    const T &operator[](int offset) const { return queue<T, SIZE>::added(offset); }
-};
-
-#ifdef SDL_BYTEORDER
-    #define endianswap16 SDL_Swap16
-    #define endianswap32 SDL_Swap32
-    #define endianswap64 SDL_Swap64
-#else
-inline ushort endianswap16(ushort n) { return (n<<8) | (n>>8); }
-inline uint endianswap32(uint n) { return (n<<24) | (n>>24) | ((n>>8)&0xFF00) | ((n<<8)&0xFF0000); }
-inline ullong endianswap64(ullong n) { return endianswap32(static_cast<uint>(n >> 32)) | ((ullong)endianswap32(static_cast<uint>(n)) << 32); }
-#endif
-
 template<class T>
 inline T endianswap(T n) { union { T t; uint i; } conv; conv.t = n; conv.i = SDL_Swap32(conv.i); return conv.t; }
 
 template<>
-inline uint endianswap<uint>(uint n) { return endianswap32(n); }
+inline uint endianswap<uint>(uint n) { return SDL_Swap32(n); }
 
 template<>
-inline int endianswap<int>(int n) { return endianswap32(n); }
+inline int endianswap<int>(int n) { return SDL_Swap32(n); }
 
 /* workaround for some C platforms that have these two functions as macros - not used anywhere */
 #ifdef getchar
