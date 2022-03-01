5a1b4863e1a4de4c1b22ae508b64f20f4af5928c
Alex Foster <123afoster@gmail.com>
2022-02-28 17:28:53 -0800

    make ents.h implementation free

--- ents.h
+++ ents.h
@@ -4,13 +4,62 @@
  *
  * The file defines static map entities of type "entity" and dynamic entities of
  * type "dynent" like players or monsters. The gamecode extends these types to
- * add game specific functionality. Note that enties with the infix "Ent_" are
- * the only static entity types dictated by the engine. The rest are gamecode
+ * add game specific functionality. Note that entities with the prefix "EngineEnt_"
+ * are the only static entity types dictated by the engine. The rest are gamecode
  * dependent.
  */
 
 #ifndef ENTS_H_
 #define ENTS_H_
+
+//constants
+constexpr int   maxents      = 10000;
+constexpr int   crouchtime   = 200;
+constexpr float crouchheight = 0.75f;
+constexpr int   maxanimparts = 3;
+//forward declarations for some internal engine parts of objects
+struct occludequery;
+class ragdolldata;
+class model;
+
+enum
+{
+    Anim_Mapmodel = 0,
+    Anim_GameSpecific
+};
+
+enum
+{
+    Anim_All        = 0x1FF,
+    Anim_Index      = 0x1FF,
+    Anim_Loop       = (1 << 9),
+    Anim_Clamp      = (1 << 10),
+    Anim_Reverse    = (1 << 11),
+    Anim_Start      = (Anim_Loop | Anim_Clamp),
+    Anim_End        = (Anim_Loop | Anim_Clamp | Anim_Reverse),
+    Anim_Dir        = 0xE00,
+    Anim_Secondary  = 12,
+    Anim_Reuse      = 0xFFFFFF,
+    Anim_NoSkin     = (1 << 24),
+    Anim_SetTime    = (1 << 25),
+    Anim_FullBright = (1 << 26),
+    Anim_NoRender   = (1 << 27),
+    Anim_Ragdoll    = (1 << 28),
+    Anim_SetSpeed   = (1 << 29),
+    Anim_NoPitch    = (1 << 30),
+    Anim_Flags      = 0xFF000000,
+};
+
+
+enum
+{
+    ClientState_Alive = 0,
+    ClientState_Dead,
+    ClientState_Spawning,
+    ClientState_Lagged,
+    ClientState_Editing,
+    ClientState_Spectator,
+};
 
 enum
 {
@@ -25,75 +74,11 @@ enum
     EngineEnt_GameSpecific,
 };
 
-struct entity                                   /**< persistent map entity */
-{
-    vec o;                                      /**< position */
-    short attr1, attr2, attr3, attr4, attr5;    /**< attributes */
-    uchar type;                                 /**< type is one of the above */
-    uchar reserved;
-};
-
 enum
 {
-    EntFlag_NoVis      = 1<<0,
-    EntFlag_NoShadow   = 1<<1,
-    EntFlag_NoCollide  = 1<<2,
-    EntFlag_Anim       = 1<<3,
-    EntFlag_ShadowMesh = 1<<4,
-    EntFlag_Octa       = 1<<5,
-    EntFlag_Render     = 1<<6,
-    EntFlag_Sound      = 1<<7,
-    EntFlag_Spawned    = 1<<8,
-
-};
-
-struct extentity : entity                       /**< part of the entity that doesn't get saved to disk */
-{
-    int flags;
-    extentity *attached;
-
-    extentity() : flags(0), attached(nullptr) {}
-
-    bool spawned() const
-    {
-        return (flags&EntFlag_Spawned) != 0;
-    }
-
-    void setspawned(bool val)
-    {
-        if(val)
-        {
-            flags |= EntFlag_Spawned;
-        }
-        else
-        {
-            flags &= ~EntFlag_Spawned;
-        }
-    }
-
-    void setspawned()
-    {
-        flags |= EntFlag_Spawned;
-    }
-
-    void clearspawned()
-    {
-        flags &= ~EntFlag_Spawned;
-    }
-};
-
-const int maxents = 10000;
-
-//extern vector<extentity *> ents;                // map entities
-
-enum
-{
-    ClientState_Alive = 0,
-    ClientState_Dead,
-    ClientState_Spawning,
-    ClientState_Lagged,
-    ClientState_Editing,
-    ClientState_Spectator,
+    PhysEnt_Player = 0,
+    PhysEnt_Camera,
+    PhysEnt_Bounce,
 };
 
 enum
@@ -108,25 +93,39 @@ enum
     PhysEntState_Bounce,
 };
 
-enum
+/**
+ * @brief Entity data capable of being saved to disk.
+ */
+struct entity                                   /**< persistent map entity */
 {
-    PhysEnt_Player = 0,
-    PhysEnt_Camera,
-    PhysEnt_Bounce,
+    vec o;                                      /**< position */
+    short attr1, attr2, attr3, attr4, attr5;    /**< attributes */
+    uchar type;                                 /**< type is one of the above */
+    uchar reserved;
 };
 
-enum
+/**
+ * @brief Extended entity data not of the subset which is saved to disk.
+ */
+struct extentity : entity
 {
-    Collide_None = 0,
-    Collide_Ellipse,
-    Collide_OrientedBoundingBox,
-    Collide_TRI
+    int flags;
+    extentity *attached;
+
+    extentity();
+
+    bool spawned() const;
+    void setspawned(bool val);
+    void setspawned();
+    void clearspawned();
 };
 
-const int   crouchtime   = 200;
-const float crouchheight = 0.75f;
-
-struct physent                                  /**< base entity type, can be affected by physics */
+/**
+ * @brief A base object representing a class
+ *
+ * Intended to be specialized for game specific functionality.
+ */
+struct physent
 {
     vec o, vel, falling;                        /**< origin, velocity */
     vec deltapos, newpos;                       /**< movement interpolation */
@@ -148,165 +147,78 @@ struct physent                                  /**< base entity type, can be af
 
     bool blocked;                               /**< used by physics to signal ai */
 
-    physent() : o(0, 0, 0), deltapos(0, 0, 0), newpos(0, 0, 0), yaw(0), pitch(0), roll(0), maxspeed(35),
-               radius(4.0f), eyeheight(14), maxheight(15), aboveeye(2), xradius(4.1f), yradius(4.1f), zmargin(0),
-               state(ClientState_Alive), editstate(ClientState_Alive), type(PhysEnt_Player),
-               collidetype(Collide_Ellipse),
-               blocked(false)
-               { reset(); }
+    physent();
 
-    void resetinterp()
-    {
-        newpos = o;
-        deltapos = vec(0, 0, 0);
-    }
-
-    void reset()
-    {
-        inwater = 0;
-        timeinair = 0;
-        eyeheight = maxheight;
-        jumping = false;
-        strafe = move = crouching = 0;
-        physstate = PhysEntState_Fall;
-        vel = falling = vec(0, 0, 0);
-        floor = vec(0, 0, 1);
-    }
-
-    vec feetpos(float offset = 0) const
-    {
-        return vec(o).addz(offset - eyeheight);
-    }
-    vec headpos(float offset = 0) const
-    {
-        return vec(o).addz(offset);
-    }
-
-    bool crouched() const
-    {
-        return fabs(eyeheight - maxheight*crouchheight) < 1e-4f;
-    }
+    void resetinterp();
+    void reset();
+    vec feetpos(float offset = 0) const;
+    vec headpos(float offset = 0) const;
+    bool crouched() const;
 };
 
-class model;
-
-struct modelattach
+/**
+ * @brief An representation of a model attachment (e.g. weapon to player).
+ */
+struct modelattach final
 {
     const char *tag, *name;
     int anim, basetime;
     vec *pos;
     model *m;
 
-    modelattach() : tag(nullptr), name(nullptr), anim(-1), basetime(0), pos(nullptr), m(nullptr) {}
-    modelattach(const char *tag, const char *name, int anim = -1, int basetime = 0) : tag(tag), name(name), anim(anim), basetime(basetime), pos(nullptr), m(nullptr) {}
-    modelattach(const char *tag, vec *pos) : tag(tag), name(nullptr), anim(-1), basetime(0), pos(pos), m(nullptr) {}
+    modelattach();
+    modelattach(const char *tag, const char *name, int anim = -1, int basetime = 0);
+    modelattach(const char *tag, vec *pos);
 };
 
-enum
-{
-    Anim_Mapmodel = 0,
-    Anim_GameSpecific
-};
-
-enum
-{
-        Anim_All        = 0x1FF,
-        Anim_Index      = 0x1FF,
-        Anim_Loop       = (1 << 9),
-        Anim_Clamp      = (1 << 10),
-        Anim_Reverse    = (1 << 11),
-        Anim_Start      = (Anim_Loop | Anim_Clamp),
-        Anim_End        = (Anim_Loop | Anim_Clamp | Anim_Reverse),
-        Anim_Dir        = 0xE00,
-        Anim_Secondary  = 12,
-        Anim_Reuse      = 0xFFFFFF,
-        Anim_NoSkin     = (1 << 24),
-        Anim_SetTime    = (1 << 25),
-        Anim_FullBright = (1 << 26),
-        Anim_NoRender   = (1 << 27),
-        Anim_Ragdoll    = (1 << 28),
-        Anim_SetSpeed   = (1 << 29),
-        Anim_NoPitch    = (1 << 30),
-        Anim_Flags      = 0xFF000000,
-};
-
-struct animinfo /**< description of a character's animation */
+/**
+ * @brief description of a character's animation
+ */
+struct animinfo final
 {
     int anim, frame, range, basetime;
     float speed;
     uint varseed;
 
-    animinfo() : anim(0), frame(0), range(0), basetime(0), speed(100.0f), varseed(0) { }
-
-    bool operator==(const animinfo &o) const
-    {
-        return frame==o.frame && range==o.range && (anim&(Anim_SetTime | Anim_Dir)) == (o.anim & (Anim_SetTime | Anim_Dir)) && (anim & Anim_SetTime || basetime == o.basetime) && speed == o.speed;
-    }
-    bool operator!=(const animinfo &o) const
-    {
-        return frame!=o.frame || range!=o.range || (anim&(Anim_SetTime | Anim_Dir)) != (o.anim & (Anim_SetTime | Anim_Dir)) || (!(anim & Anim_SetTime) && basetime != o.basetime) || speed != o.speed;
-    }
+    animinfo();
+    bool operator==(const animinfo &o) const;
+    bool operator!=(const animinfo &o) const;
 };
 
-struct animinterpinfo /**< used for animation blending of animated characters */
+/**
+ * @brief used for animation blending of animated characters
+ */
+struct animinterpinfo final
 {
     animinfo prev, cur;
     int lastswitch;
     void *lastmodel;
 
-    animinterpinfo() : lastswitch(-1), lastmodel(nullptr) {}
-
-    void reset() { lastswitch = -1; }
+    animinterpinfo();
+    void reset();
 };
 
-const int maxanimparts = 3;
-
-struct occludequery;
-class ragdolldata;
-
-struct dynent : physent                         /**< animated characters, or characters that can receive input */
+/**
+ * An extension of dynent with mobile/animated model handling
+ */
+struct dynent : physent
 {
-    bool k_left, k_right, k_up, k_down;         /**< see input code */
+    bool k_left,    /**< whether keyboard left movement is enabled */
+         k_right,   /**< whether keyboard right movement is enabled */
+         k_up,      /**< whether keyboard up movement is enabled */
+         k_down;    /**< whether keyboard down movement is enabled */
 
     animinterpinfo animinterp[maxanimparts];
     ragdolldata *ragdoll;
     occludequery *query;
     int lastrendered;
 
-    dynent() : ragdoll(nullptr), query(nullptr), lastrendered(0)
-    {
-        reset();
-    }
+    dynent();
+    ~dynent();
 
-    ~dynent()
-    {
-        extern void cleanragdoll(dynent *d);
-        if(ragdoll)
-        {
-            cleanragdoll(this);
-        }
-    }
-
-    void stopmoving()
-    {
-        k_left = k_right = k_up = k_down = jumping = false;
-        move = strafe = crouching = 0;
-    }
-
-    void reset()
-    {
-        physent::reset();
-        stopmoving();
-        for(int i = 0; i < maxanimparts; ++i)
-        {
-            animinterp[i].reset();
-        }
-    }
-
-    vec abovehead()
-    {
-        return vec(o).addz(aboveeye+4);
-    }
+    void stopmoving();
+    void reset();
+    vec abovehead();
 };
 
 #endif /* ENTS_H_ */
