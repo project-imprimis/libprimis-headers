d0ab3a00995935d680438848c56e7723c4bddfff
pietrorinaldiunive <898756@stud.unive.it>
2023-11-11 12:07:14 +0100

    added optionals for sound

--- sound.h
+++ sound.h
@@ -63,7 +63,7 @@ class SoundEngine
          * @param radius distance over which sound is audible (in size 0 cubes)
          * @param expire maximum time before the sound stops playing
          */
-        int playsound(int n, const vec *loc = nullptr, extentity *ent = nullptr, int flags = 0, int loops = 0, int fade = 0, int chanid = -1, int radius = 0, int expire = -1);
+        std::optional<int> playsound(int n, const vec *loc = nullptr, extentity *ent = nullptr, int flags = 0, int loops = 0, int fade = 0, int chanid = -1, int radius = 0, int expire = -1);
 
         /**
          * @brief Stops playing a sound of index n in the specificied channel.
@@ -415,7 +415,7 @@ class SoundEngine
             std::vector<SoundConfig> configs;
             const char *dir;
             SoundType(const char *dir, SoundEngine& p);
-            int findsound(const char *name, int vol);
+            std::optional<int> findsound(const char *name, int vol);
             int addslot(const char *name, int vol);
             int addsound(const char *name, int vol, int maxuses = 0);
             void addalt(const char *name, int vol);
